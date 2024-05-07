deb75c887b5fdf44f837ef732beb728d8fe67ed1
Danilo Pinto <62811338+Danilo-Pinto@users.noreply.github.com>
2024-05-07 16:55:50 -0300

    Update index.js

--- api/core/index.js
+++ api/core/index.js
@@ -1,25 +1,23 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware para permitir o uso de JSON no corpo das requisições
+// Middleware para permitir o uso de JSON no corpo das requisiÃ§Ãµes
 app.use(express.json());
 
 let users = [];
 
-// Rota para listar todos os usuários
+// Rota para listar todos os usuÃ¡rios
 app.get('/users', (req, res) => {
     res.json(users);
 });
 
-// Rota para criar um novo usuário
+// Rota para criar um novo usuÃ¡rio
 app.post('/users', (req, res) => {
     const newUser = req.body;
     users.push(newUser);
-    res.status(201).send('Usuário criado com sucesso.');
+    res.status(201).send('UsuÃ¡rio criado com sucesso.');
 });
 
-// Iniciar o servidor
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+module.exports.handler = serverless(app);
