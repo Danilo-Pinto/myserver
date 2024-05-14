f7dc05833f6ee6f9e7b68456060bd5ce942b503b
Danilo Pinto <62811338+Danilo-Pinto@users.noreply.github.com>
2024-05-14 09:13:18 -0300

    Update index.js

--- api/core/index.js
+++ api/core/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const PORT = process.env.PORT || 3000;
-// Middleware para permitir o uso de JSON no corpo das requisições
+
 app.use(express.json());
 let users = [];
+
 // Rota para listar todos os usuários
 app.get('/users', (req, res) => {
     res.json(users);
@@ -12,9 +13,19 @@ app.get('/users', (req, res) => {
 
 // Rota para criar um novo usuário
 app.post('/users', (req, res) => {
-    const {user} = req.body;
+    const { user } = req.body;
     users.push(user);
     res.status(201).send('Usuário criado com sucesso.');
 });
+
+// Função para adicionar um novo usuário a cada minuto
+function addNewUser() {
+    const newUser = { name: 'Novo Usuário', email: 'novousuario@example.com' };
+    users.push(newUser);
+    console.log('Novo usuário adicionado:', newUser);
+}
+
+// Executa a função para adicionar um novo usuário a cada minuto
+setInterval(addNewUser, 60000); // 60000 milissegundos = 1 minuto
 
 module.exports.handler = serverless(app);
